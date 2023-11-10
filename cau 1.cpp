a8b6a4b1d9288efa008050d64d51d60e2c8ee1e8
nqa1903 <103617099+nqa1903@users.noreply.github.com>
2023-11-10 09:30:51 +0700

    cau 1.cpp

--- cau 1.cpp
+++ cau 1.cpp
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include <math.h>
 
-
 int main(){
 	int a , b , c;
 	float x1 , x2;
 	printf("nhap a b c :");
 	scanf("%d %d %d",&a ,&b ,&c);
 	float dental = b*b - 4*a*c;
-	if(a == 0 && b == 0 && c == 0){
+	if(a == 0 && b == 0 & c == 0){
 		printf("phuong trinh vo so nghiem");
-	}
-	if(dental < 0){
-		printf("phuong trinh vo nghiem");
-	}else if(dental ==0){
-		x1 = -b/(2*a);
-		printf("niem phuong trinh : %.2f", x1);
-	}else{
-		x1 = (-b + sqrt(dental)) / (2*a);
-		x2 = (-b - sqrt(dental)) / (2*a);
-		printf("niem cua phuong trinh la %.2f va %.2f",x1,x2);
+	}else if(a == 0 && b != 0){
+		printf("niem phuong trinh la %.2f" , (float)-c/b);
+	}else if(a != 0 ){
+		if(dental < 0){
+			printf("phuong trinh vo nghiem");
+		}else if(dental ==0){
+			x1 = -b/(2*a);
+			printf("niem phuong trinh : %.2f", x1);
+		}else{
+			x1 = (-b + sqrt(dental)) / (2*a);
+			x2 = (-b - sqrt(dental)) / (2*a);
+			printf("niem cua phuong trinh la %.2f va %.2f",x1,x2);
+		}
 	}
 }
